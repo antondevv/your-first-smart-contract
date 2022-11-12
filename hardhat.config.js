54dbbdd5d4418b646440360fe3dc0fc1c11ce9df
Anton Franzen <anton@exploreistic.com>
2022-11-13 00:26:36 +0100

    feat: use environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,15 @@
+require("dotenv").config()
 require("@nomiclabs/hardhat-ethers")
-const ALCHEMY_API_KEY = "your alchemy api key"
-const GOERLI_PRIVATE_KEY =
-  "your secret privat Goerli wallet key, never show this to anyone, and do not push it to github"
+
+const { ALCHEMY_API_URL, PRIVATE_KEY } = process.env
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {},
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [GOERLI_PRIVATE_KEY],
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_URL}`,
+      accounts: [PRIVATE_KEY],
     },
   },
 }
